e9dbbc972403679c963fb116a4383642c8c6069f
PedroBozan <pedroobozan@gmail.com>
2022-08-11 22:06:27 -0300

    Commit projeto

--- exibirDadosCep.php
+++ exibirDadosCep.php
@@ -11,19 +11,70 @@ $dadosCep->consultarCep($_POST['cep']);
 <!DOCTYPE html>
 <html lang="pt-br">
     <head>
+
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width,initial-scale=1.0">
-        <link rel="stylesheet" type="text/css" href="css/estilo.css">
+
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Oxygen:wght@700&display=swap" rel="stylesheet">
+        <link rel="icon" type="image/x-icon" href="app/img/favicon.ico">
+        <link rel="stylesheet" type="text/css" href="app/css/exibir.css">
+        <link rel="stylesheet" type="text/css" href="app/bootstrap/css/bootstrap.min.css">
+
+        <script type="text/javascript" src="app/js/script.js"></script>
         
-        <title>Exibir dados CEP</title>
+        <title>Consulta CEP</title>
     </head>
+
     <body class="body">
-        <input type="text" value="<?=$dadosCep->numCep?>" name="cep">
-        <input type="text" value="<?=$dadosCep->descLogradouro?>" name="cep">
-        <input type="text" value="<?=$dadosCep->descComplemento?>" name="cep">
-        <input type="text" value="<?=$dadosCep->descBairro?>" name="cep">
-        <input type="text" value="<?=$dadosCep->descCidade?>" name="cep">
-        <input type="text" value="<?=$dadosCep->descEstado?>" name="cep">
-        <input type="text" value="<?=$dadosCep->codIbge?>" name="cep">
+
+        <header>
+            <h1>Resultado</h1>
+        </header>
+        
+        <section>
+
+            <div class="campo">
+                <label for="cep">CEP:</label>
+                <input type="text" id="cep "name="cep" class="form-control shadow-sm p-2" value="<?=$dadosCep->numCep?>" readonly>
+            </div>
+                
+            <div class="campo">
+                <label for="logradouro">Logradouro:</label>
+                <input type="text" id="logradouro" name="logradouro" class="form-control shadow-sm p-2" value="<?=$dadosCep->descLogradouro?>" readonly>
+            </div>
+
+            <div class="campo">
+                <label for="complemento">Complemento:</label>
+                <input type="text" id="complemento" name="complemento" class="form-control shadow-sm p-2" value="<?=$dadosCep->descComplemento?>" readonly>
+            </div>
+
+            <div class="campo">
+                <label for="bairro">Bairro:</label>
+                <input type="text" id="bairro" name="bairro" class="form-control shadow-sm p-2" value="<?=$dadosCep->descBairro?>" readonly>
+            </div>
+
+            <div class="campo">
+                <label for="cidade">Cidade:</label>
+                <input type="text" id="cidade" name="cidade" class="form-control shadow-sm p-2" value="<?=$dadosCep->descCidade?>" readonly>
+            </div>
+            
+            <div class="campo">
+                <label for="estado">Estado:</label>
+                <input type="text" id="estado" name="estado" class="form-control shadow-sm p-2" value="<?=$dadosCep->descEstado?>" readonly>
+            </div>
+
+            <div class="campo">
+                <label for="ibge">Código IBGE:</label>
+                <input type="text" id="ibge" name="ibge" class="form-control shadow-sm p-2" value="<?=$dadosCep->codIbge?>" readonly>
+            </div>
+
+        </section>
+
+        <footer>
+            <p>© 2022 Pedro Bozan - Todos os Direitos Reservados.</p>
+        </footer>
+
     </body>
 </html>
--- index.php
+++ index.php
@@ -1,31 +1,41 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
     <head>
+
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width,initial-scale=1.0">
+
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Oxygen:wght@700&display=swap" rel="stylesheet">
         <link rel="icon" type="image/x-icon" href="app/img/favicon.ico">
-        <link rel="stylesheet" type="text/css" href="app/css/estilo.css">
+        <link rel="stylesheet" type="text/css" href="app/css/index.css">
         <link rel="stylesheet" type="text/css" href="app/bootstrap/css/bootstrap.min.css">
         
         <title>Consulta CEP</title>
     </head>
+
     <body class="body">
 
-        <div>
-            <img class="logo" src="app/img/logo.png" alt="logo">
-        </div>
-
-        <div class="container-md">
+        <header>
+            <h1>Consulta <span>CEP</span></h1>
+        </header>
+            
+        <section>
             <form action="exibirDadosCep.php" method="POST">
-                <fieldset class="camposForm">
-                    <div class="campoCep">
-                        <input type="text" name="cep" class="form-control shadow-sm p-2" id="cep" placeholder="Digite o cep" maxlength="9" required>
-                    </div>
-                    <div class="botaoConsulta">
-                        <button id="botao" class="btn btn-dark shadow-sm" type="submit">Consultar</button>
-                    </div>
-                </fieldset>
+                <div class="campoCep">
+                    <input type="text" id="cep" name="cep" class="form-control shadow-sm p-2" placeholder="Digite o cep" maxlength="9" required>
+                </div>
+                <div>
+                    <button id="botao" class="btn btn-dark shadow-sm" type="submit">Consultar</button>
+                </div>
             </form>
-        </div>
+        </section>
+
+        <footer>
+            <p>© 2022 Pedro Bozan - Todos os Direitos Reservados.</p>
+        </footer>
+        
     </body>
 </html>
